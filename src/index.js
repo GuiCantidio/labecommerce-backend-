841daf25511c46fde6163a6e6db7734dd3bfaff9
Guilherme_Cantidio <guilherme_cantidio@hotmail.com>
2023-04-13 14:19:55 -0300

    exercicio Apis_Express Project

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import {Tuser,product,Tcompra,} from "./database"
+
+// console.log(Tuser);
+// console.log(product);
+// cobsole.log(Tcompra);
--- src/type.ts
+++ src/type.ts
@@ -1,20 +1,93 @@
-export type Tcliente ={
-    id: string,
-    email:string,
-    senha:string,
-}
 
-export type Tproduto ={
-    id: string,
-    nome:string,
-    preco:number,
-    categoria:string
+
+export type TUser ={
+   id: string,
+   nome?:string,
+   email:string,
+   senha:string,
+   createAt?:string
 };
 
-export type Tcompra ={
-    userId: string,
-    produtoId: string,
-    quantidade:number,
-    valorTotal: number
+
+export type createUsers ={
+  id: string,
+  email:string,
+  senha: string
 }
 
+export type  products={
+   id: string,
+  nome:string,
+   preco:number,
+  categoria:CATEGORY
+};
+
+export type Tcompra={
+  userId: string,
+  produtoId: string,
+  quantidade:number,
+  valorTotal:number
+}
+export enum CATEGORY{
+  ACCESSORIES = "Acessórios",
+  CLOTHES_AND_SHOES = "Roupas e calçados",
+  ELECTRONICS = "Eletrônicos",
+
+}
+
+
+
+
+// export type Tcliente ={
+//     id: string,
+//     email:string,
+//     senha:string,
+// }
+
+// export type Tproduto ={
+//     id: string,
+//     nome:string,
+//     preco:number,
+//     categoria:string
+// };
+
+// export type Tcompra ={
+//     userId: string,
+//     produtoId: string,
+//     quantidade:number,
+//     valorTotal: number
+// }
+
+// export type TUser ={
+//     id: string,
+//     nome?:string,
+//     email:string,
+//     senha:string,
+//     createAt?:string
+// };
+
+
+// export type createUsers ={
+//    id: string,
+//    email:string,
+//    senha: string
+// }
+
+// export type  products={
+//     id: string,
+//    nome:string,
+//     preco:number,
+//    categoria:CATEGORY
+// };
+
+// export type Tcompra ={
+//    userId: string,
+//    produtoId: string,
+//    quantidade:number,
+//    valorTotal:number
+// }
+// export enum CATEGORY{
+//    ACCESSORIES = "Acessórios",
+//    CLOTHES_AND_SHOES = "Roupas e calçados",
+//    ELECTRONICS = "Eletrônicos"
+// }
